f6c701dbb0e77d829a40aa16182a916b1fa2b04e
sebtn <tonomudo@gmail.com>
2015-11-10 19:49:25 +0000

    More valdations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,11 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validate :name, :presence => true
+	validate :name,  presence: true
+
+	belongs_to :places
+	validates :name, presence: true, length: { minimum: 3}
+	validates :address, presence: true
+	validates :description, presence: true
+
 end
 
