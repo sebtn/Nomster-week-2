57ec5d95fa90727ac053aaf7dccb6ce8b862a3be
sebtn <tonomudo@gmail.com>
2015-11-11 17:19:02 +0000

    Submit button to save V2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!	
 	def create
 		@place = Place.find(params[:place_id])
-		@place.comments.create(comment_params)
+		@place.comments.create(comment_params.merge(:user => current_user))
 		redirect_to place_path(@place)
 	end	
 
