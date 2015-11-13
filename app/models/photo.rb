367b82fad94f99149cbc4097b9e9e44010280934
sebtn <tonomudo@gmail.com>
2015-11-13 23:04:59 +0000

    Photos button modal not working

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
-
+	belongs_to :user
+	belongs_to :place
+	
 	mount_uploader :picture, PictureUploader
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Nomster::Application.routes.draw do
   root 'places#index'
   resources :places do
     resources :comments, :only => :create
-    resources :photos
+    resources :photos, :only => :create
   end 
 
   
