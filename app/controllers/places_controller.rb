c0b3abc6e9a03b1a5664a7eb3812eb905a391565
sebtn <tonomudo@gmail.com>
2015-11-18 23:35:47 +0000

    Carousel v1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
-	   @places = Place.all.page(params[:page]).per(10)
+	   @places = Place.all.page(params[:page]).per(7)
 	end	
 
 	def new
